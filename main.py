12dc4e068bc6ad41254a527d144de307b0fc0ad7
PraxTube <lukinator28@gmail.com>
2024-01-14 17:02:24 +0100

    feat!: Invert the Jacobi matrix properly to allow multi dim

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 def newton(F, dF, x0, delta=1e-04, ep=1e-04, maxIter=100):
     x_k = x0
+
     for i in range(maxIter):
-        x = x_k - F(x_k) / dF(x_k)
+        x = x_k - np.linalg.inv(dF(x_k)) @ F(x_k)
         if np.linalg.norm(F(x_k)) < ep:
             break
         if np.linalg.norm(x - x_k) < delta:
@@ -19,9 +20,28 @@ def plot_simple_function():
     """
     Calculate x_0 for f: R -> R with newtons method
     """
-    F = lambda x: x**3 - 2 * x
-    dF = lambda x: 3 * x**2 - 2
-    x0s = (0.1, 2, -2)
+
+    def F(x):
+        x = np.array(x)
+        y = np.array(x**3 - 2 * x).reshape(1, 1)
+        return y
+
+    def dF(x):
+        x = np.array(x)
+        y = np.array(3 * x**2 - 2).reshape(1, 1)
+        return y
+
+    x0s = [
+        np.array(0.1).reshape(
+            1,
+        ),
+        np.array(2).reshape(
+            1,
+        ),
+        np.array(-2).reshape(
+            1,
+        ),
+    ]
     delta = 1e-10
     ep = 1e-10
     maxIter = 50
@@ -29,7 +49,7 @@ def plot_simple_function():
     x0_values = np.array([newton(F, dF, x0, delta, ep, maxIter) for x0 in x0s])
     y0_values = np.zeros(x0_values.shape[0])
     x_values = np.linspace(-2, 2, 100)
-    y_values = np.array([F(x) for x in x_values])
+    y_values = np.squeeze(np.array([F(x) for x in x_values]))
 
     # Plotting
     plt.figure(figsize=(14, 6))
@@ -42,8 +62,33 @@ def plot_simple_function():
     plt.show()
 
 
+def plot_second_function():
+    """
+    f: R**2 -> R**2
+    """
+
+    def F(x):
+        x = np.array(x)
+        y = np.array((x[0] ** 2 + x[1] ** 2 - 6 * x[0], 3 / 4 * np.exp(-x[0]) - x[1]))
+        return y
+
+    def dF(x):
+        x = np.array(x)
+        y = np.array((2 * x[0] + 2 * x[1] - 6 * x[0], 3 / 4 * np.exp(-x[0]) - x[1]))
+        return y
+
+    x0s = (0.1, 2, -2)
+    delta = 1e-10
+    ep = 1e-10
+    maxIter = 50
+
+    x0_values = np.array([newton(F, dF, x0, delta, ep, maxIter) for x0 in x0s])
+    y0_values = np.zeros(x0_values.shape[0])
+
+
 def main():
     plot_simple_function()
+    # plot_second_function()
 
 
 if __name__ == "__main__":
